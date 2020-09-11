72c7ba25e0a1a39d6d7d4a69116a0bc9c9025368
xLikeWATCHCAT <bilibili.mer@gmail.com>
2020-09-11 17:28:17 +0800

    优化引入类

--- src/main/java/top/dsbbs2/bukkitcord/api/ITabCompleter.kt
+++ src/main/java/top/dsbbs2/bukkitcord/api/ITabCompleter.kt
@@ -1,5 +1,7 @@
 package top.dsbbs2.bukkitcord.api
 
+import kotlin.jvm.Throws
+
 interface ITabCompleter {
     @Throws(IllegalArgumentException::class)
     fun tabComplete(sender: ICommandSender?, command: ICommand?, alias: String?, args: Array<String?>?): List<String?>?
