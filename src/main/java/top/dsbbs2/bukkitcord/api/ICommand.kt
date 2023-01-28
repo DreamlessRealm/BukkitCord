d55802a47b18d32eab3302e0464041673fb66c67
lvyitian <15988566900@qq.com>
2023-01-28 20:18:44 +0800

    Update and rename ICommand.kt to ICommand.java

--- src/main/java/top/dsbbs2/bukkitcord/api/ICommand.kt
+++
@@ -1,9 +0,0 @@
-package top.dsbbs2.bukkitcord.api
-
-interface ICommand {
-    var permission: String?
-    var platformAliases: Array<String?>?
-    var platformExecutor: ICommandExecutor?
-    var platformUsage: String?
-    var platformTabCompleter: ITabCompleter?
-}
