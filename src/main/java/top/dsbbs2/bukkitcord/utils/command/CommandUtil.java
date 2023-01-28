dd6639eb728521e9c3cea5ff37358e2f6fe071e4
lvyitian <15988566900@qq.com>
2023-01-28 19:36:39 +0800

    Update and rename CommandUtil.kt to CommandUtil.java

---
+++ src/main/java/top/dsbbs2/bukkitcord/utils/command/CommandUtil.java
@@ -0,0 +1,20 @@
+package top.dsbbs2.bukkitcord.utils.command
+
+import java.util.*
+
+public class CommandUtil {
+    public static Vector<String> commandListToCommandNameList(Vector<IChildCommand> v) {
+        Vector<String> ret = new Vector<String>();
+        for(i : v) {
+            ret.add(i.class.getSimpleName());
+        }
+        return ret;
+    }
+
+    public static IChildCommand getCommand(Vector<IChildCommand> l, String cmd) {
+        for(i : l) {
+            if (i.class.getSimpleName().equalsIgnoreCase(cmd)) return i;
+        }
+        return null;
+    }
+}
