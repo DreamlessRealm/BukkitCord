15fcbe4d9bddf885c99428c0e01f52b7b3a61879
lvyitian <15988566900@qq.com>
2023-01-28 20:20:13 +0800

    Update and rename ICommandExecutor.kt to ICommandExecutor.java

--- src/main/java/top/dsbbs2/bukkitcord/api/ICommandExecutor.kt
+++
@@ -1,6 +0,0 @@
-package top.dsbbs2.bukkitcord.api
-
-@FunctionalInterface
-interface ICommandExecutor {
-    fun execute(sender: ICommandSender?, command: ICommand?, args: Array<String?>?): Boolean
-}
